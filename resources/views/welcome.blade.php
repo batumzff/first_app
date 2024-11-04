2708ab92aaaee664550d6e0c4c8e1c6d7c6ace53
batumzff <batuhanmuzafferoglu@gmail.com>
2024-11-04 19:27:30 +0300

    adding css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Coder Network</title>
+    
+    @vite('resources/css/app.css')
 </head>
-<body>
+<body class="px-8 py-12 text-center" >
     <h1>Welcome to the Batu Network</h1>
     <p>Click the button below to view the list of coders.</p>
 
-    <a href="/coders" class="btn">
+    <a href="/coders" class="inline-block mt-4 btn">
     Find Coders!</a>
 </body>
 </html>
